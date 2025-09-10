e1f8f18412615bf47fc1ba72741164ce291ee5b5
ppisod <hicnkebromzih@gmail.com>
2025-09-10 13:17:49 +0800

    MinimumDateAge method + impl

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Dateable {
 
+    public static int MinimumDateAge (int age) {
+        return (int) ((int) (age / 2d) + 7d);
+    }
+
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        String ageString = scanner.nextLine(); // nextInt
+        int age = Integer.parseInt(ageString);
+        System.out.println("Your minimum date age IS: " + MinimumDateAge(age));
+
     }
 }
